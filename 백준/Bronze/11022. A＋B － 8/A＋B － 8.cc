8c3410881dced113d86d25f513d3b99ef17ed8ba
parksooo <104567063+parksooo@users.noreply.github.com>
2022-06-26 20:33:21 +0900

    [Bronze III] Title: A+B - 8, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, t;
+    scanf("%d", &t);
+    for(int i = 0; i < t; i++){
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", i + 1, a, b, a + b);
+    }
+    return 0;
+}
