c8a135424bbd1362c23cca74a5293957919128a6
parksooo <104567063+parksooo@users.noreply.github.com>
2022-06-26 14:06:03 +0900

    [Bronze III] Title: 합, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.cc
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a, sum;
+    sum = 0;
+    scanf("%d", &a);
+    for(int i = 0; i <= a; i++){
+        sum = sum + i;
+    }
+    printf("%d", sum);
+    return 0;
+}
