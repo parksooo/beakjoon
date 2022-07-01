8de72e658163fe64a576e01ccaa1062713a39c56
parksooo <104567063+parksooo@users.noreply.github.com>
2022-07-01 17:41:59 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 1972 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    int i, len, cnt = 0;
+    char arr[1000000];
+    
+    scanf("%[^\n]", arr);
+    len = strlen(arr);
+    
+    if(len == 1)
+    {
+    	if(arr[0] == ' ')
+        {
+            printf("0\n");
+    		return 0;
+        }
+    }
+    
+    for(i = 1; i < len-1; i++)
+    {
+        if(arr[i] == ' ')
+            cnt++;
+    }
+    
+    printf("%d\n", cnt + 1);
+    return 0;
+}
